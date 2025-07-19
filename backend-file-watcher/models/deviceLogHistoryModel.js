b4399dc4a30949c53a3502d7581979ec05b5bd60
Ash-Dev <developers@asherdn.com>
2025-07-19 10:40:58 +0530

    update model with customer ID

--- backend-file-watcher/models/deviceLogHistoryModel.js
+++ backend-file-watcher/models/deviceLogHistoryModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const historySchema = new mongoose.Schema({
+  customerId: { type: mongoose.Schema.Types.ObjectId, ref: "CustomerUser" },
   DeviceID: Number,
   Connected: String,
   Date: String,
