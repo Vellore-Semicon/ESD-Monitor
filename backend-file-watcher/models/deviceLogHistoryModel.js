0b0f0854632b378ae6454787d4f6c444d9918681
Ash-Dev <developers@asherdn.com>
2025-07-21 17:59:42 +0530

    schema update

--- backend-file-watcher/models/deviceLogHistoryModel.js
+++ backend-file-watcher/models/deviceLogHistoryModel.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const historySchema = new mongoose.Schema({
   customerId: { type: mongoose.Schema.Types.ObjectId, ref: "CustomerUser" },
+  masterDeviceId: { type: mongoose.Schema.Types.ObjectId, ref: "MasterDevice" },
   DeviceID: Number,
+  deviceName: String,
   Connected: String,
   Date: String,
   Time: String,
@@ -12,10 +14,18 @@ const historySchema = new mongoose.Schema({
   Mat1: String,
   Mat2: String,
   Machine: String,
+  isVisible: { type: Boolean, default: true },
   FetchedAt: {
     type: Date,
     default: Date.now,
   },
+});
+
+historySchema.pre("save", function (next) {
+  if (this.DeviceID && !this.deviceName) {
+    this.deviceName = `DEV-${this.DeviceID}`;
+  }
+  next();
 });
 
 module.exports = mongoose.model("DeviceLogHistory", historySchema);
--- backend-file-watcher/models/deviceLogModel.js
+++ backend-file-watcher/models/deviceLogModel.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const logSchema = new mongoose.Schema({
   customerId: { type: mongoose.Schema.Types.ObjectId, ref: "CustomerUser" },
+  masterDeviceId: { type: mongoose.Schema.Types.ObjectId, ref: "MasterDevice" },
   DeviceID: Number,
+  deviceName: String,
   Connected: String,
   Date: String,
   Time: String,
@@ -12,7 +14,18 @@ const logSchema = new mongoose.Schema({
   Mat1: String,
   Mat2: String,
   Machine: String,
-  FetchedAt: { type: Date, default: Date.now },
+  isVisible: { type: Boolean, default: true },
+  FetchedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+logSchema.pre("save", function (next) {
+  if (!this.deviceName && this.DeviceID != null) {
+    this.deviceName = `DEV-${this.DeviceID}`;
+  }
+  next();
 });
 
 module.exports = mongoose.model("DeviceLog", logSchema);
